4a55de5323479c275c352f7a544c5326fa0265b6
Wyyyyyyc <375714307@qq.com>
2025-09-09 15:54:56 +0800

    feat: add frontend news page

---
+++ frontend/pages/news.tsx
@@ -0,0 +1,11 @@
+export default function News() {
+  return (
+    <div>
+      <h1>新闻资讯</h1>
+      <ul>
+        <li>2025-09-01 公司发布新产品</li>
+        <li>2025-08-15 荣获行业大奖</li>
+      </ul>
+    </div>
+  )
+}
