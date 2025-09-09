cccfe8de85b46bc630e79e056e065bb643519e8b
Wyyyyyyc <375714307@qq.com>
2025-09-09 15:55:19 +0800

    feat: add frontend Footer component

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2025 某某公司 版权所有</p>
+    </footer>
+  )
+}
