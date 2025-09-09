11096b4c009aae32bfebacc758d77ee2d27ced63
Wyyyyyyc <375714307@qq.com>
2025-09-09 15:54:43 +0800

    feat: add frontend products page

---
+++ frontend/pages/products.tsx
@@ -0,0 +1,12 @@
+export default function Products() {
+  return (
+    <div>
+      <h1>产品中心</h1>
+      <ul>
+        <li>产品一</li>
+        <li>产品二</li>
+        <li>产品三</li>
+      </ul>
+    </div>
+  )
+}
